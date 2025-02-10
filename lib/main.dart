fe7ff3be6096a02e9f3ae8c05fbb4fa052fdc7f8
amandatheuri <amandatheuri19@gmail.com>
2025-02-10 18:27:01 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,33 @@
-// ignore_for_file: unused_import
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:myapp/core/utils/device/deviceutility.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+//import 'package:shared_preferences/shared_preferences.dart';
 import 'app.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(); 
+
   final bool isDarkMode = WidgetsBinding.instance.window.platformBrightness == Brightness.dark;
-   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
-    statusBarColor: isDarkMode?  Colors.transparent:Colors.grey, 
-    statusBarIconBrightness: isDarkMode? Brightness.light: Brightness.dark,
-    systemNavigationBarColor: isDarkMode? Colors.black: Colors.white, 
-    systemNavigationBarIconBrightness: isDarkMode? Brightness.light:Brightness.dark, 
-  ));
-  //WidgetsFlutterBinding.ensureInitialized();
-  //final prefs = await SharedPreferences.getInstance();
-  //bool onboardingComplete = prefs.getBool('onboardingComplete') ?? false;
-  //bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: isDarkMode ? Colors.transparent : Colors.grey,
+      statusBarIconBrightness: isDarkMode ? Brightness.light : Brightness.dark,
+      systemNavigationBarColor: isDarkMode ? Colors.black : Colors.white,
+      systemNavigationBarIconBrightness: isDarkMode ? Brightness.light : Brightness.dark,
+    ),
+  );
+
+  // Uncomment if you need onboarding/login state
+  // final prefs = await SharedPreferences.getInstance();
+  // bool onboardingComplete = prefs.getBool('onboardingComplete') ?? false;
+  // bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
   runApp(
     MyApp(
-      //onboardingComplete: onboardingComplete,
-      //isLoggedIn: isLoggedIn,
-    )
+      // onboardingComplete: onboardingComplete,
+      // isLoggedIn: isLoggedIn,
+    ),
   );
 }
--- lib/navigation_bar.dart
+++ lib/navigation_bar.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:myapp/core/utils/constants/colors.dart';
 import 'package:myapp/controllers/navcontroller.dart';
+import 'package:myapp/core/utils/constants/colors.dart';
 
 class NavBar extends StatelessWidget {
   const NavBar({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final controller = Get.put(NavController());
     return Scaffold(
       bottomNavigationBar: Obx(
         () => NavigationBar(
-          height: 70,
-        elevation: 3,
-        selectedIndex: controller.selectedIndex.value,
-        indicatorColor: TColors.primaryColor,
-        backgroundColor: Colors.transparent,
-        onDestinationSelected: (index) => controller.selectedIndex.value = index,
-          destinations: [
-            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Rewards'),
-            NavigationDestination(icon: Icon(Iconsax.trash), label: 'Dispose'),
-            NavigationDestination(icon: Icon(Iconsax.shield), label: 'Challenges'),
-              ] 
-        
-        ),
+            height: 70,
+            elevation: 3,
+            selectedIndex: controller.selectedIndex.value,
+            indicatorColor: TColors.primaryColor,
+            backgroundColor: Colors.transparent,
+            onDestinationSelected: (index) =>
+                controller.selectedIndex.value = index,
+            destinations: [
+              NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+              NavigationDestination(icon: Icon(Iconsax.shop), label: 'Rewards'),
+              NavigationDestination(
+                  icon: Icon(Iconsax.trash), label: 'Dispose'),
+              NavigationDestination(
+                  icon: Icon(Iconsax.shield), label: 'Challenges'),
+            ]),
       ),
-      body: Obx(() =>controller.screens[controller.selectedIndex.value]),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
 }
